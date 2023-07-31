b51563d63c615595904b85015b8f822f65caf5d7
mattcondie22 <140651033+mattcondie22@users.noreply.github.com>
2023-07-31 16:46:59 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -172,7 +172,7 @@ export const managers = [
       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
          {
-      "roster": 9,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "roster": null,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : "595761692734980096",      
       "name": "Sal Galan",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
