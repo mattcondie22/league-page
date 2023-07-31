9a623843b52ca66b1b8cf101c8487816e53a06db
mattcondie22 <140651033+mattcondie22@users.noreply.github.com>
2023-07-31 14:09:38 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -23,6 +23,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
     {
        "roster": null,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "managerID" : "451323900492509184",
       "name": "Your Name",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Brooklyn", // (optional)
