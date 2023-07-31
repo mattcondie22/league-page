a58ea6351a2cd283b80f5b642785f9e9992a8fdb
mattcondie22 <140651033+mattcondie22@users.noreply.github.com>
2023-07-31 13:54:43 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -22,7 +22,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
     {
-      "managerID" : "451323900492509184"
+      "managerID" : "451323900492509184",
       "name": "Your Name",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Brooklyn", // (optional)
