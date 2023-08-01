1fd474500b964ae105b8ccc8df2a1c7fa1c2550e
mattcondie22 <140651033+mattcondie22@users.noreply.github.com>
2023-07-31 22:47:03 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -156,7 +156,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Nashville, TN", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/name.tristen", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/tristen.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "rival": {
