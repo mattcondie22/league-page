bb38f7f5130a89d5490417dc78ea592c43fb8eb0
mattcondie22 <140651033+mattcondie22@users.noreply.github.com>
2023-08-02 11:52:45 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -155,7 +155,7 @@ export const managers = [
       "name": "Tristen Coates-Carter",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Nashville, TN", // (optional)
-      "bio": "Bio: Tristen has to work every Sunday but that doesn't stop him from getting lit af and crushing his opponents every week in fantasy. First b2b league winner, bringing home a 3rd ship this year after an unlucky year on the 3Peat.",
+      "bio": "Bio: Tristen has to work every Sunday but that doesn't stop him from getting lit af and crushing his opponents every week in fantasy. First b2b league winner, bringing home a 3rd ship this year after an unlucky year on the 3Pete.",
       "photo": "/managers/tristen.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
